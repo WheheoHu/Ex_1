86ef6464ec7f20f12eb077d8510721b3047b2b15
文昊 胡 <hwh19981124@hotmail.com>
2019-03-08 09:01:42 +0800

    finish

--- Ex_1/Main.cpp
+++ Ex_1/Main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+
 #include "glut.h"
 
 
 
-#pragma comment(lib,"opengl32.lib")
-#pragma comment(lib,"glu32.lib")
+//#pragma comment(lib,"opengl32.lib")
+//#pragma comment(lib,"glu32.lib")
 #pragma comment(lib,"glut32.lib")
 
 
